9c0b050e5d550b59c942db529ab9cfa2bd28050c
Felipe <felipe.smg336@gmail.com>
2026-04-24 15:18:31 -0400

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/felipeSMG/greetings
+
+go 1.26.1
---
+++ greetings.go
@@ -0,0 +1,10 @@
+package greteings
+
+import (
+	"fmt"
+)
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hola, %v", name)
+	return message
+}
